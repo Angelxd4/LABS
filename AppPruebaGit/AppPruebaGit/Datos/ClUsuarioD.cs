a79d96c305fbd5525fcb79726c33f8debe668d9f
Valerie <gonzalezvalerie938@gmail.com>
2025-10-29 20:51:46 -0500

    Se creo el metodo listar

--- AppPruebaGit/AppPruebaGit/Datos/ClUsuarioD.cs
+++ AppPruebaGit/AppPruebaGit/Datos/ClUsuarioD.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using AppPruebaGit.Modelo;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+
 
 namespace AppPruebaGit.Datos
 {
     public class ClUsuarioD
     {
+        Usuario user = new Usuario();
+        ClConexion oConexion = new ClConexion();
+        public List<Usuario> listar()
+        {
+            List<Usuario> lista = new List<Usuario>();
+            SqlConnection conexion = oConexion.MtAbrir();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Usuario");
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                lista.Add(new Usuario()
+                {
+                    documento = dr.GetString(0),
+                    nombre = dr.GetString(1),
+                    apellido = dr.GetString(2),
+                    email = dr.GetString(3)
+                });
+            }
+            return lista;
+
+
+        }
     }
 }
