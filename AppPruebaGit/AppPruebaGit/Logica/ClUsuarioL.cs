f50b684b77985c771181b7d2f1d2fc247f3ff105
Valerie <gonzalezvalerie938@gmail.com>
2025-10-29 22:34:36 -0500

    Se agrego la clase ClUsuarioL en la carpeta Logica y en esta se crearon los metodos CRUD

---
+++ AppPruebaGit/AppPruebaGit/Logica/ClUsuarioL.cs
@@ -0,0 +1,36 @@
+﻿using AppPruebaGit.Datos;
+using AppPruebaGit.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppPruebaGit.Logica
+{
+    public class ClUsuarioL
+    {
+        ClUsuarioD datos = new ClUsuarioD();
+
+        public List<Usuario> Listar()
+        {
+            return datos.listar();
+        }
+        public bool Registrar(Usuario u)
+        {
+            return datos.Registrar(u);
+        }
+        public bool Editar(Usuario u)
+        {
+            return datos.Editar(u);
+        }
+        public bool Eliminar(int documento)
+        {
+            return datos.Eliminar(documento);
+        }
+        public Usuario Buscar(int documento)
+        {
+            return datos.Buscar(documento);
+        }
+
+    }
+}
